1b7d73d11856c7ca640b3b692b60fd308b73f1b6
Silas <silas.oliveira8@gmail.com>
2022-08-09 16:27:51 -0300

    feat: req 01

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  private static instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race.instances += 1;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): never | number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
