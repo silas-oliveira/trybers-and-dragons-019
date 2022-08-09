6aa698b2f0053cab0e73904b74ced4bb0e6e2ef3
Silas <silas.oliveira8@gmail.com>
2022-08-09 19:37:38 -0300

    feat: req 06

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// import { Dwarf, Elf } from './Races';
-
-// const t = new Dwarf('ingrid', 288).createdRacesInstances;
-// console.log('Dwarfffff', t);
-// const t2 = new Dwarf('ingrid', 288).createdRacesInstances;
-// console.log(t2);
-
-// const tt = new Elf('ingrid', 288).createdRacesInstances;
-// console.log('Elfffffff', tt);
